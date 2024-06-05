c9de9dbc22144186fabec546eb681c492ca6aadf
Dennis Akagha <d.akagha20@gmail.com>
2024-06-05 01:25:34 +0100

    add sending email functionality

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,4 @@
-from django.forms import forms
+from django import forms
 
 
 # form method
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 from blog.forms import EmailPostForm
 from .models import Post
@@ -51,16 +52,34 @@ def post_detail(request, year, month, day, post):
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
 
+    sent = False
+
     if request.method == "POST":
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = (
+                f"{cd['name']} ({cd['email']}) " f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd["to"]],
+            )
+            sent = True
     else:
         form = EmailPostForm()
 
     context = {
         "post": post,
         "form": form,
+        "sent": sent,
     }
 
     return render(request, "blog/post/share.html", context)
